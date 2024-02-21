aa5dab4368c5bc89107cfba8f5a2ec1cd29dd013
Brijesh Singh <101446302+brijesh2004@users.noreply.github.com>
2024-02-21 16:55:51 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-const GetVideo = ()=> {
-    const iframe_id = document.getElementById('iframe_id');
-    const input  = document.getElementById("inputId").value;
-    let url = input.slice(23,input.length);
-    url = `https://www.youtube.com/embed/${url}`;
-    console.log(url);
-    iframe_id.src=url;
-}
